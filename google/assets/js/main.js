914f267fcc80ce25fb0e5ccbade62a3ae88f5669
carlosrailson <carlosrailsondev@gmail.com>
2021-09-29 12:51:36 -0300

    atualizando o projeto com a alteração de vários arquivos

--- google/assets/js/main.js
+++ google/assets/js/main.js
@@ -5,14 +5,17 @@ function prevenirEnvio(e) {
 const button = document.querySelectorAll('button')
 addEventListener('click', prevenirEnvio) // previne o envio do formulário ao clicar nos ícones que estão dentro de buttons
 
-function apps() {
-  const modal = document.querySelector('.container-modal')
-  modal.style.display = 'flex'
+function containerModal() {
+  const modal = document.querySelector('.container-modal') 
+  modal.classList.add('mostrar')
+  modal.addEventListener('click', function(e) {
+    if (e.target == modal) {
+      modal.classList.remove('mostrar')
+    }
+  })
 }
 
-function containerModal() {
-  const modal = document.querySelector('.container-modal')
-  if (modal.style.display = 'flex') {
-    modal.style.display = 'none'
-  }
-} // É preciso corrigir esta parte do código, pois está acontecendo um bug. Mesmo clicando apenas no 'modal', o 'container-modal' está sendo afetado e ao invés de fechar apenas uma div, acaba fechando as duas;
+const apps = document.querySelector('.apps')
+apps.addEventListener('click', function() {
+  containerModal() 
+})
